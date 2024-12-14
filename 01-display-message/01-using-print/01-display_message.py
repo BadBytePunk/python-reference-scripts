bf0e2f9899072703babddd2c83be3833a420e9a0
BadBytePunk <badbytepunk@gmail.com>
2024-12-14 16:44:01 -0500

    Add files via upload

---
+++ 01-display-message/01-using-print/01-display_message.py
@@ -0,0 +1,9 @@
+# 01-display_message.py
+# BadBytePunk
+# 2024-12-13
+
+#### DISPLAY MESSAGE ####
+
+""" ++++ BASIC PRINT STATEMENTS ++++ """
+# Print Simple Message
+print("Hello! This is a simple message.") # print function to display the string beetween (" ") 
