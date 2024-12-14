b39d37196066bdf769362eda806edf9e40f751ec
BadBytePunk <badbytepunk@gmail.com>
2024-12-14 16:43:41 -0500

    Delete 01- display-message/01-using-print directory

--- 01- display-message/01-using-print/01-display_message.py
+++
@@ -1,9 +0,0 @@
-# 01-display_message.py
-# BadBytePunk
-# 2024-12-13
-
-#### DISPLAY MESSAGE ####
-
-""" ++++ BASIC PRINT STATEMENTS ++++ """
-# Print Simple Message
-print("Hello! This is a simple message.") # print function to display the string beetween (" ") 
